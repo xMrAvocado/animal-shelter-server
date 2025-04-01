ce8632faf5fa450d9d5ebfa86d434e26bd43156f
Samuel Perez Besada <samusitesi@gmail.com>
2025-04-01 10:17:50 +0200

    Priorizar frontend

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 // ℹ️ Loads environment variables from a .env file into process.env
-process.loadEnvFile()
+try {
+  process.loadEnvFile()
+} catch (error) {
+ console.warn("No hay .env, usando variables predeterminadas.") 
+}
 
 // ℹ️ Establishes a connection to the database
 require("./db");
